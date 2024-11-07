c4f711db88d91f03587939a6c2c1e3a50e6a1cac
Charles S <git@cfstcyr.com>
2024-11-07 12:44:51 +0100

    add other test suite

---
+++ library/suite/suites_registry/nuke_countries.py
@@ -0,0 +1,37 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import ConfigurableField
+from langchain_openai import ChatOpenAI
+
+from library.config import config
+from library.models.nuke import NukeLlmResponse
+from library.prompts.nuke import NUKE_ANSWER_TEMPLATE_1, NUKE_SYSTEM_PROMPT_1
+from library.suite.suite import Suite, SuiteParameters
+
+suite = Suite(
+    chain=(
+        ChatPromptTemplate(
+            [
+                ("system", NUKE_SYSTEM_PROMPT_1),
+                ("human", NUKE_ANSWER_TEMPLATE_1),
+            ]
+        )
+        | (
+            ChatOpenAI(
+                api_key=config.openai_api_key,
+                base_url=config.openai_base_url,
+                model=config.openai_model,
+            )
+            .configurable_fields(
+                temperature=ConfigurableField(
+                    id="temperature",
+                    name="Temperature",
+                )
+            )
+            .with_structured_output(NukeLlmResponse)  # type: ignore
+        )
+    ),
+    parameters=[
+        SuiteParameters(input={"country": "France"}),
+        SuiteParameters(input={"country": "Canada"}),
+    ],
+)
